3f4d4702e93f5125737fd3f288787f5be5ff6fa3
fabiocozzolino <cozzolino.fabio@gmail.com>
2019-11-24 18:03:40 +0100

    create new meetup and required model

--- Data/Meetup.cs
+++ Data/Meetup.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blazor.EventPlanner.Data
 {
     public class Meetup
     {
         public Guid Id {get;set;}
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public string Summary { get; set; }
+        public string Description { get; set; }
+        [Required]
         public string Speaker { get; set; }
         public List<MeetupAttendee> Attendees {get;set;}=new List<MeetupAttendee>();
     }
 
     public class MeetupAttendee
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
     }
 }
--- Data/MeetupService.cs
+++ Data/MeetupService.cs
@@ -15,6 +15,7 @@ namespace Blazor.EventPlanner.Data
                 Id = Guid.NewGuid(),
                 Date = DateTime.Now.AddDays(5),
                 Summary = "Async/Await in C#",
+                Description="The Async-Await pattern has now become ubiquitous in C# development, however it still causes confusion for many developers. This is partly because of the amount of misinformation about it floating around and partially because of the lack of more advanced examples of its usage. David Allen and Ben Kotvis from BlueMetal will talking about how Async-Await works and some of the lesser known features and patterns that are useful in the real world. Amongst the topics they will cover are: Tasks, what they are, and options for creating them; What the Async-Await pattern is and how to use it, return types from async methods, and the When, Wait, Continue, and Result Task extensions; Creating async methods from synchronous methods; Synchronization Context, what it means and how to manipulate it; and how to use Async-Await and still get results back from queues and events handlers using TaskCompletionSource. Hopefully after this presentation attendees will have a better understanding of the Async-Await pattern, and be confident enough to start investigating and using some of the more advanced features available in the various Asynchronous patterns supported in .Net.",
                 Speaker = "Fabio Cozzolino"
             });
             Meetups.Add(new Meetup{
