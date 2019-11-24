d82968b23c8bfb20aeadd37725f8d69300a8a095
fabiocozzolino <cozzolino.fabio@gmail.com>
2019-11-24 17:09:13 +0100

    first release

---
+++ Data/Meetup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blazor.EventPlanner.Data
+{
+    public class Meetup
+    {
+        public Guid Id {get;set;}
+        public DateTime Date { get; set; }
+        public string Summary { get; set; }
+        public string Speaker { get; set; }
+        public List<MeetupAttendee> Attendees {get;set;}=new List<MeetupAttendee>();
+    }
+
+    public class MeetupAttendee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Data/MeetupService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.EventPlanner.Data
+{
+    public class MeetupService
+    {
+        private static readonly List<Meetup> Meetups = new List<Meetup>();
+
+        public MeetupService()
+        {
+            Meetups.Add(new Meetup{
+                Id = Guid.NewGuid(),
+                Date = DateTime.Now.AddDays(5),
+                Summary = "Async/Await in C#",
+                Speaker = "Fabio Cozzolino"
+            });
+            Meetups.Add(new Meetup{
+                Id = Guid.NewGuid(),
+                Date = DateTime.Now.AddDays(12),
+                Summary = "MVVM Concepts",
+                Speaker = "Daniele Romanazzi"
+            });
+            Meetups.Add(new Meetup{
+                Id = Guid.NewGuid(),
+                Date = DateTime.Now.AddDays(19),
+                Summary = "Fabio Cozzolino",
+                Speaker = ".NET Core 3"
+            });
+        }
+
+        public Task<Meetup[]> GetForecastAsync(DateTime startDate)
+        {
+            return Task.FromResult(Meetups.ToArray());
+        }
+
+        public void CreateMeetup(DateTime date, string summary, string speaker)
+        {
+            Meetups.Add(new Meetup{
+                Id = Guid.NewGuid(),
+                Date=date,
+                Summary=summary,
+                Speaker=speaker
+            });
+        }
+
+        public void AddAttendee(Guid meetupId, MeetupAttendee attendee)
+        {
+            var currentMeetup = Meetups.FirstOrDefault(m => m.Id == meetupId);
+            currentMeetup.Attendees.Add(attendee);
+        }
+
+        public Meetup GetMeetup(Guid meetupId)
+        {
+            return Meetups.FirstOrDefault(m => m.Id == meetupId);
+        }
+    }
+}
